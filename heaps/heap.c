b77261bf6f38dfde9b92aa5a0389fe637e2fd95f
preslavgerchev <preslavgerchevmail@gmail.com>
2018-12-05 18:42:16 +0100

    WIP onheaps.

--- heaps/heap.c
+++ heaps/heap.c
@@ -1,56 +1,138 @@
+#include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 #include "array.h"
 #include "prioq.h"
 
-static
-struct heap* heap_init(int (*compare)(const void*, const void*)) {
-    // ... CODE MISSING HERE ....
-
-    // Store compare the function pointer argument in the struct if
-    // the struct pointer is called 'h':
-    // h->compare = compare;
-
-    // ... CODE MISSING HERE ....
+int swap(struct heap *h, int first_index, int second_index) {
+  void *first = array_get(h->array, first_index);
+  void *second = array_get(h->array, second_index);
+  if (first == NULL || second == NULL) {
+    return 1;
+  }
+  array_set(h->array, second_index, first);
+  array_set(h->array, first_index, second);
+  return 0;
 }
 
-struct heap* prioq_init(int (*compare)(const void*, const void*)) {
-    return heap_init(compare);
+int left(int index) { return index * 2 + 1; }
+
+int right(int index) { return index * 2 + 2; }
+
+int parent(int index) { return (index - 1) / 2; }
+
+static struct heap *heap_init(int (*compare)(const void *, const void *)) {
+  prioq *h = malloc(sizeof(prioq));
+  if (h == NULL)
+    return NULL;
+  struct array *a = array_init(10);
+  if (a == NULL)
+    return NULL;
+  h->array = a;
+  h->compare = compare;
+  return h;
 }
 
-long int prioq_size(struct heap *h) {
-    // ... CODE MISSING HERE ....
+struct heap *prioq_init(int (*compare)(const void *, const void *)) {
+  return heap_init(compare);
 }
 
-static
-int heap_cleanup(struct heap *h, void free_func(void*)) {
-    // ... CODE MISSING HERE ....
+long int prioq_size(struct heap *h) { return array_size(h->array); }
+
+static int heap_cleanup(struct heap *h, void free_func(void *)) {
+  if (h == NULL || h->array == NULL) {
+    return 1;
+  }
+  array_cleanup(h->array, free_func);
+  free(h);
+  return 0;
 }
 
-int prioq_cleanup(prioq *h, void free_func(void*)) {
-    // ... CODE MISSING HERE ....
+int prioq_cleanup(prioq *h, void free_func(void *)) {
+  return heap_cleanup(h, free_func);
 }
 
-static
-int heap_insert(struct heap *h, void *p) {
-    // ... CODE MISSING HERE ....
+static int heap_insert(struct heap *h, void *p) {
+  int index = (int)prioq_size(h);
+  int set = array_append(h->array, p);
+  if (set == -1) {
+    return -1;
+  }
 
-    // To compare items in the heap these heaps functions can use the stored
-    // compare function pointer in the following way:
-    //  if (h->compare(arg1, arg1)) { ...
+  if (prioq_size(h) == 1) {
+    return 0;
+  }
+
+  while (index > 0) {
+    int parent_index = parent(index);
+    void *parent = array_get(h->array, parent_index);
+    void *current = array_get(h->array, index);
+    int res = h->compare(current, parent);
+    if (res < 0) {
+      swap(h, index, parent_index);
+      index = parent_index;
+    } else {
+      // Else if the parent is higher then simply break the while loop.
+      break;
+    }
+  }
+  return 0;
 }
 
-int prioq_insert(prioq *h, void *p) {
-    return heap_insert(h, p);
+int prioq_insert(prioq *h, void *p) { return heap_insert(h, p); }
+
+static void *heap_pop(struct heap *h) {
+  int size = (int)prioq_size(h);
+  if (size == 0) {
+    return NULL;
+  }
+  if (swap(h, 0, size - 1) == 1) {
+    return NULL;
+  }
+  void *val = array_pop(h->array);
+  int index = 0;
+  // If the queue is either empty or contains a single element there is no need
+  // for sorting.
+  if (prioq_size(h) == 1) {
+    return val;
+  }
+  void *current_val = array_get(h->array, index);
+  void *right_val = array_get(h->array, right(index));
+  void *left_val = array_get(h->array, left(index));
+  // Due to the fact that there may be nodes with a single leaf, first check if
+  // there is at least one leaf that can be compared against.
+  while (current_val != NULL && (right_val != NULL || left_val != NULL)) {
+    bool swap_left = false;
+    bool swap_right = false;
+    int swap_index;
+
+    if (left_val != NULL) {
+      swap_left = h->compare(current_val, left_val) > 0;
+    }
+    if (right_val != NULL) {
+      swap_right = h->compare(current_val, right_val) > 0;
+    }
+    // If the current cannot be swaped with either of the children then break.
+    if (!swap_left && !swap_right) {
+      break;
+    }
+    // If both children can be swapped choose the lower one.
+    if (swap_left && swap_right) {
+      swap_index =
+          h->compare(left_val, right_val) < 0 ? left(index) : right(index);
+    } else {
+      swap_index = swap_left ? left(index) : right(index);
+    }
+    swap(h, index, swap_index);
+    index = swap_index;
+    current_val = array_get(h->array, index);
+    right_val = array_get(h->array, right(index));
+    left_val = array_get(h->array, left(index));
+  }
+
+  return val;
 }
 
-static
-void* heap_pop(struct heap *h) {
-    // ... CODE MISSING HERE ....
-}
-
-void* prioq_pop(prioq *h) {
-    return heap_pop(h);
-}
+void *prioq_pop(prioq *h) { return heap_pop(h); }
--- heaps/main.c
+++ heaps/main.c
@@ -1,9 +1,9 @@
 #include <fcntl.h>
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <getopt.h>
 
 #include "prioq.h"
 
@@ -12,84 +12,103 @@
 static char buf[BUF_SIZE];
 
 struct config {
-    // You can ignore these options until/unless you implement the
-    // bonus features.
+  // You can ignore these options until/unless you implement the
+  // bonus features.
 
-    // Set to 1 if -y is specified, 0 otherwise.
-    int year;
+  // Set to 1 if -y is specified, 0 otherwise.
+  int year;
 };
 
-static
-int parse_options(struct config *cfg, int argc, char *argv[]);
+static int parse_options(struct config *cfg, int argc, char *argv[]);
 
 typedef struct {
-    char *name;
-    // ... CODE MISSING HERE ....
+  char *name;
+  // ... CODE MISSING HERE ....
 } patient_t;
 
-static int compare_patient_name(const void *a, const void *b)
-{
-    return strcmp(((const patient_t *) a)->name,
-            ((const patient_t *) b)->name);
+static int compare_patient_name(const void *a, const void *b) {
+  return strcmp(((const patient_t *)a)->name, ((const patient_t *)b)->name);
 }
 
-static int compare_patient_age(const void *a, const void *b)
-{
-    // ... CODE MISSING HERE ....
+static int compare_patient_age(const void *a, const void *b) { return 0; }
+
+void free_patient(void *patient) {
+  patient_t *p = (patient_t *)patient;
+  free(p->name);
+  free(p);
 }
 
 int main(int argc, char *argv[]) {
-    char *token, *name_cpy;
-    prioq *queue;
-    struct config cfg;
+  char *token, *name_cpy;
+  prioq *queue;
+  struct config cfg;
 
-    if (parse_options(&cfg, argc, argv) != 0) {
+  if (parse_options(&cfg, argc, argv) != 0) {
+    return EXIT_FAILURE;
+  }
+
+  if (cfg.year) {
+    queue = prioq_init(&compare_patient_age);
+  } else {
+    queue = prioq_init(&compare_patient_name);
+  }
+  char *spec_char = ".\n";
+  for (int iterations = 0;;) {
+    while (1) {
+      char *s = fgets(buf, BUF_SIZE, stdin);
+      if (s == NULL) {
+        fprintf(stderr, "Unexpected end of file. exiting\n");
         return EXIT_FAILURE;
-    }
-
-    if (cfg.year) {
-        queue = prioq_init(&compare_patient_age);
-    } else {
-        queue = prioq_init(&compare_patient_name);
-    }
-
-    for (int iterations = 0;;) {
-        while (1) {
-            char *s = fgets(buf, BUF_SIZE, stdin);
-            if (s == NULL) {
-                fprintf(stderr, "Unexpected end of file. exiting\n");
-                return EXIT_FAILURE;
-            }
-
-            // ... CODE MISSING HERE ....
-
+      }
+      if (strcmp(spec_char, s) == 0) {
+        patient_t *popped = (patient_t *)prioq_pop(queue);
+        if(popped != NULL) {
+          printf("%s", popped->name);
         }
-
-        printf(".\n"); // End turn.
-
-        if (++iterations == 10) {
-
-            // ... CODE MISSING HERE ....
-
-            break;
-        }
+        break;
+      } else {
+        patient_t *p = malloc(sizeof(patient_t));
+        if (p == NULL)
+          return EXIT_FAILURE;
+        p->name = malloc(strlen(s) * sizeof(char) + 2);
+        if (p->name == NULL)
+          return EXIT_FAILURE;
+        strcpy(p->name, s);
+        int res = prioq_insert(queue, p);
+        if (res == 1)
+          return EXIT_FAILURE;
+      }
     }
 
-    // ... CODE MISSING HERE ....
+    printf(".\n"); // End turn.
 
-    return EXIT_SUCCESS;
+    if (++iterations == 10) {
+      patient_t *popped = (patient_t *)prioq_pop(queue);
+      while (popped != NULL) {
+        printf("%s", popped->name);
+        popped = (patient_t *)prioq_pop(queue);
+      }
+      break;
+    }
+  }
+
+  prioq_cleanup(queue, free_patient);
+
+  return EXIT_SUCCESS;
 }
 
 int parse_options(struct config *cfg, int argc, char *argv[]) {
-    memset(cfg, 0, sizeof (struct config));
-    int c;
-    while ((c = getopt (argc, argv, "y")) != -1) {
-        switch (c) {
-            case 'y': cfg->year = 1; break;
-            default:
-                fprintf(stderr, "invalid option: -%c\n", optopt);
-                return 1;
-        }
+  memset(cfg, 0, sizeof(struct config));
+  int c;
+  while ((c = getopt(argc, argv, "y")) != -1) {
+    switch (c) {
+    case 'y':
+      cfg->year = 1;
+      break;
+    default:
+      fprintf(stderr, "invalid option: -%c\n", optopt);
+      return 1;
     }
-    return 0;
+  }
+  return 0;
 }
